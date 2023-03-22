9d2fed2231c5c9c8dfaf3c94c09ae51e1dc8e6f2
Sooraaz <sooraaz@gmail.com>
2023-03-22 00:52:20 -0500

    Upload files

---
+++ src/main/java/net/javaguides/springboot/dto/UserDto.java
@@ -0,0 +1,29 @@
+package net.javaguides.springboot.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+    private Long id;
+    //User first name should not be null or empty
+    @NotEmpty(message="User first name should not be null or empty")
+    private String firstName;
+
+    //User last name should not be null or empty
+    @NotEmpty(message = "User last name should not be null or empty")
+    private String lastName;
+
+    //User email name should not be null or empty
+    //email should be valid
+    @NotEmpty(message = "User email name should not be null or empty")
+    @Email(message = "Email address should be valid")
+    private String email;
+}
